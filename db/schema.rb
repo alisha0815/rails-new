e1ed660852daf7ff1bd174dc72af9e3bba686cdb
alisha <aliciasohn815@gmail.com>
2021-07-12 00:08:05 +0200

    modifed schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_200317) do
+ActiveRecord::Schema.define(version: 2021_05_03_124305) do
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.text "comment"
+    t.integer "movie_id", null: false
+    t.integer "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
+  end
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -22,4 +32,6 @@ ActiveRecord::Schema.define(version: 2021_05_01_200317) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
